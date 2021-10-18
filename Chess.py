702c81ee781ebaf464e116fdd172bd2fb89c543e
paologum <pgumasin@purdue.edu>
2021-10-18 17:52:31 -0400

    Fixed formatting issues and added more descriptions for each turn. Added TODO for minimax algorithim

--- Chess.py
+++ Chess.py
@@ -18,7 +18,7 @@ def print_move_list():
             move_list += str(int(count / 2) + 1) + ". "
         move_list += m + " "
         count += 1
-    return move_list
+    return move_list + "\n\n========================"
 
 #pick AI difficulty
 while not pick_difficulty:
@@ -37,9 +37,12 @@ while not pick_color:
     except ValueError:
         print("Unfortunately, that is not an option, please pick again.")
         pick_color = False
+
+#prints initial board
+print("\n========================")
 print("\nWhite to move\n")
 print(board)
-print("\n")
+print("\n========================\n")
 
 #game info, AI moves, and user input for moves are selected here until game is over
 while not board.is_checkmate() or not board.is_stalemate() or not board.is_insufficient_material() or resign:
@@ -47,6 +50,7 @@ while not board.is_checkmate() or not board.is_stalemate() or not board.is_insuf
     if ((board.turn == chess.WHITE and user_color == 1) or (board.turn == chess.BLACK and user_color == 2)) :
         while not move in board.legal_moves:
             move = input("Please specify your move in SAN: ")
+            print()
             try : 
                 move = board.parse_san(move)
             except ValueError:
@@ -54,9 +58,11 @@ while not board.is_checkmate() or not board.is_stalemate() or not board.is_insuf
                 print("Invalid Input")
             if not move == chess.Move.null() and not move in board.legal_moves :
                 print("Invalid Input")
+
+    #AI moves for easy mode
     elif ai_difficulty == 1:
         #TODO legal moves that work with pins, checks, etc
-        random = randint(0, board.legal_moves.count())
+        random = randint(0, board.legal_moves.count() - 1)
         count = 0
         for legal_move in board.legal_moves :
             if random == count :
@@ -64,17 +70,26 @@ while not board.is_checkmate() or not board.is_stalemate() or not board.is_insuf
                 break
             else :
                 count += 1
+
+    #TODO AI moves for medium difficulty (minimax and alpha beta pruning)
     elif ai_difficulty == 2:
         print("difficulty medium")
 
 # pushes whatever move and continues the game
     total_moves.append(board.san(move))
     board.push(move)
-    if (user_color == 2 and board.turn == chess.BLACK) or (user_color == 1 and board.turn == chess.WHITE):
-        #TODO print whose turn it is
-        print(" turn to move\n")
-        print(board)
-        print("\n")
+    if (board.turn == chess.BLACK):
+        color = "Black's"
+    else :
+        color = "White's"
+    if ((board.turn == chess.BLACK and user_color == 2) or (board.turn == chess.WHITE and user_color == 1)):
+        ai_or_user = "(Your turn)"
+    else :
+        ai_or_user = "(Computer's turn)"
+    print(color + " turn to move " + ai_or_user + "\n")
+    print(board)
+    print()
     print(print_move_list())
-    if (((board.turn == chess.BLACK and user_color == 1) or (board.turn == chess.WHITE and user_color == 2) and input("Resign? Type Yes or No.").lower() == "no")) :
+    if (((board.turn == chess.BLACK and user_color == 1) or (board.turn == chess.WHITE and user_color == 2) and input("Resign? Type Yes or No: ").lower() == "no")) :
+        print()
         resign = True
